fb72438a19637734e24539dc1ae4e5ca7705bdd1
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2022-09-26 19:41:13 +0300

    Create Snowballs.cs

---
+++ Exercise Data Types and Variables/Snowballs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11._Snowballs
+{
+    internal class Snowballs
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger maxSum = int.MinValue;
+            int maxSnSn = 0;
+            int maxSnTm = 0;
+            int maxSnQu = 0;
+
+            int snowballSnow = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                
+
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality); 
+
+                if (snowballValue >= maxSum)
+                {
+                    maxSnSn = snowballSnow;
+                    maxSnTm = snowballTime;
+                    maxSnQu = snowballQuality;
+                    maxSum = snowballValue;
+                }
+
+                if (i == n)
+                {
+                    break;
+                }
+
+                else
+                {
+                    snowballSnow = int.Parse(Console.ReadLine());
+                    snowballTime = int.Parse(Console.ReadLine());
+                    snowballQuality = int.Parse(Console.ReadLine());
+                }
+
+                snowballValue = 0;
+            }
+            Console.WriteLine($"{maxSnSn} : {maxSnTm} = {maxSum} ({maxSnQu})");
+        }
+    }
+}
