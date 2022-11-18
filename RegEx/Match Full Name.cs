2b9fe3b4cebd137bbed5cc997671003eb615e0b0
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2022-11-18 18:05:43 +0200

    Adding Exercises

---
+++ RegEx/Match Full Name.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _1._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection result = regex.Matches(input);
+
+            foreach (Match match in result)
+            {
+                Console.Write($"{match.Value} ");
+            }
+        }
+    }
+}
---
+++ RegEx/Match Phone Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+\b359(-| )2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+
+            //foreach (Match match in matchCollection)
+            //{
+            //    Console.Write(match.Value + ", ");
+            //}
+
+
+            string[] result = matchCollection.Select(x => x.Value).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
